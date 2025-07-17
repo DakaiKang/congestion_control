ee85f91566c277242825a68a86544b474a45cb25
Ubuntu <ubuntu@ip-172-31-71-168.ec2.internal>
2025-07-17 03:36:51 +0000

    pevm as a dependency in mysticeti

--- mysticeti/crates/mysticeti-core/src/transactions_generator.rs
+++ mysticeti/crates/mysticeti-core/src/transactions_generator.rs
@@ -5,6 +5,7 @@ use std::{cmp::min, sync::Arc, time::Duration};
 
 use rand::{rngs::StdRng, Rng, SeedableRng};
 use tokio::sync::mpsc;
+use pevm::Pevm;
 
 use crate::{
     config::{ClientParameters, NodePublicConfig},
